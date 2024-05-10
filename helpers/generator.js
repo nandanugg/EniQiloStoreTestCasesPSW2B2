46d73d9633ef43984674376a339f29b4f5845942
nandanugg <nandanugg@gmail.com>
2024-05-10 15:08:45 +0700

    update

--- helpers/generator.js
+++ helpers/generator.js
@@ -82,10 +82,11 @@ export function generateRandomDescription(maxLength = 20) {
     const loremIpsum =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    const startIndex = generateRandomNumber(0, loremIpsum.length - maxLength);
-    const description = loremIpsum.substring(startIndex, startIndex + maxLength);
-
-    return description;
+    if (loremIpsum.length <= maxLength) {
+        return loremIpsum;
+    } else {
+        return loremIpsum.substring(loremIpsum.length - maxLength);
+    }
 }
 
 
